75410559c48475692018ff2aaf2e3bf43e400c6a
ToonVanDeun <toon.vandeun@student.kuleuven.be>
2014-05-12 20:47:57 +0200

    nog wat zitten proberen

---
+++ src/worms/model/programs/PartExpression.java
@@ -0,0 +1,16 @@
+package worms.model.programs;
+
+public class PartExpression {
+	
+	private Expression left;
+	private Expression right;
+	
+	public Expression getLeftExpression() {
+		return this.left;
+	}
+	
+	public Expression getRightExpression() {
+		return this.left;
+	}
+}
+
