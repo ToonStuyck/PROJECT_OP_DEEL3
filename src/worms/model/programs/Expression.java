219abdf5f6b0851e5bd5472ea6eed1703d3aa1f8
ToonVanDeun <toon.vandeun@student.kuleuven.be>
2014-05-12 20:48:37 +0200

    nog wat zitten proberen

--- src/worms/model/programs/Expression.java
+++ src/worms/model/programs/Expression.java
@@ -1,7 +1,22 @@
 package worms.model.programs;
 
-public interface Expression<T> {
-	
-	public T getExpression();	
+import worms.model.Expression;
 
+public class Expression {
+	
+	public Expression(int line, int column) {
+		this.line = line;
+		this.column = column;
+	}
+
+	private int line;
+	private int column;
+	private PartExpression partExpression;
+	
+	public void createLogicAnd(Expression e1, Expression e2) {
+		this.partExpression = new LogicAnd(e1,e2);
+	}
+	
+
+	
 }
